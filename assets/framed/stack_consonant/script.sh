d7916f1dfc1ad93e2e27f1dfc312144b86695611
Rotsarak Kang <rotsarak.kang@gmail.com>
2022-10-28 15:44:43 +0700

    assets sample

---
+++ assets/framed/stack_consonant/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare -a NAME=("ka" "kha" "ko" "kho" "ngo" "cha" "chha" "cho" "chho" "nho" "da" "tha1" "do" "tho1" "na" "ta" "tha2" "to" "tho2" "no" "ba" "pha" "po" "pho" "mo" "yo" "ro" "lo" "vo" "sa" "ha" "la" "or");
+
+for i in "${NAME[@]}"
+do
+    mkdir "stack_${i}"
+done
--- assets/shrinked/stack_consonant/stack_kho/README.md
+++
@@ -1,2 +0,0 @@
-
-stack_kho
