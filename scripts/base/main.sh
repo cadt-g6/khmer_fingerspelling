e4ab57213c0f5f876b24c49f4d95b5e936a4e4a8
Thea Choem <theacheng.g6@gmail.com>
2022-11-13 22:25:25 +0700

    feat: add cnn with EfficientNetB0 + refactoring

--- scripts/base/main.sh
+++
@@ -1,6 +0,0 @@
-rm -rf datasets/csv
-mkdir datasets/csv
-
-find scripts/independent_vowels/ -name "*.sh" -exec {} \;
-find scripts/sub_vowels/ -name "*.sh" -exec {} \;
-find scripts/vowels/ -name "*.sh" -exec {} \;
